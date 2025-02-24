a4c31f5940f4302d482177fbcea42054839b1008
Kieran Grant <>
2025-02-24 21:00:28 +0000

    finished chapter 5.3

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"runtime/debug"
 )
+
+func (app *application) render(
+	w http.ResponseWriter,
+	r *http.Request,
+	status int,
+	page string,
+	data templateData) {
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(status)
+
+	err := ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
 	var (
