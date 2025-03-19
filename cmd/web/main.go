ef80a144d0530e0650307b0fc3efc156ed87fe51
Kieran Grant <>
2025-03-19 20:02:54 +0000

    finished chapter 9.4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -58,9 +58,15 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	srv := &http.Server{
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err = http.ListenAndServe(*addr, app.routes())
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 }
