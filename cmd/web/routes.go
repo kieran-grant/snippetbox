ec8a6f801729f546aaa33c7fb8387c9738b8b2a3
Kieran Grant <>
2025-03-25 13:45:48 +0000

    finished chapter 12

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,15 @@ package main
 import (
 	"net/http"
 
+	"snippetbox/ui"
+
 	"github.com/justinas/alice"
 )
 
-// The routes() method returns a servemux containing our application routes.
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
